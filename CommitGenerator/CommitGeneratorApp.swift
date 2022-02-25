9e2c372a4426d40acff191736b8611b293f30bbb
Park <pjh00098@gmail.com>
2022-02-25 13:11:19 +0900

    first commit

---
+++ CommitGenerator/CommitGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitGeneratorApp.swift
+//  CommitGenerator
+//
+//  Created by 박종호 on 2022/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CommitGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
